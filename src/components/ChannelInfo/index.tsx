73128b6146e0b54dbf27bc8d53098997a747e14c
Rodrigo Ferreira Schaer <rodrigo.fscs@gmail.com>
2021-10-14 12:07:04 -0300

    adds channel header

---
+++ src/components/ChannelInfo/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import {
+  Container,
+  HashtagIcon,
+  Title,
+  Separator,
+  Description,
+} from "./styles";
+
+const ChannelInfo: React.FC = () => {
+  return (
+    <Container>
+      <HashtagIcon />
+      <Title>sac</Title>
+      <Separator />
+      <Description>reclame aqui</Description>
+    </Container>
+  );
+};
+
+export default ChannelInfo;
